a0c99e74c93ebf77bcaf809248a22b3d9d6bb075
omegafrog <jiwoo22110103@gmail.com>
2026-03-19 11:49:29 +0900

    feat: 리스트 카드에 혼잡도 문구 표시 추가

--- TrafficForm/Domain/TrafficLevel.cs
+++ TrafficForm/Domain/TrafficLevel.cs
@@ -8,4 +8,22 @@ namespace TrafficForm.Domain
         Heavy = 3,
         Congested = 4
     }
+    
+    public static class TrafficLevelExtensions
+    {
+    public static string ToDisplayString(this TrafficLevel level)
+        {
+            return level switch
+            {
+                TrafficLevel.Unknown => "확인불가",
+                TrafficLevel.Smooth => "원활",
+                TrafficLevel.Slow => "보통",
+                TrafficLevel.Heavy => "혼잡",
+                TrafficLevel.Congested => "정체",
+                _ => level.ToString()
+            };
+        }
+    }
+    
+
 }
